9ae7643fcdc93049ddedc68b1cface6eb0abd6c5
DinukaWalpola <dinukawalpola1@gmail.com>
2024-06-10 21:13:40 +0530

    Optimized the code

--- LeetCode/Easy/ReverseStringSolution.cs
+++ LeetCode/Easy/ReverseStringSolution.cs
@@ -4,22 +4,11 @@ public static class ReverseStringSolution
 {
     public static char[] ReverseString(char[] s) 
     {
-        for (int i = 0; i < s.Length; i++)
+        for (int i = 0; i < s.Length / 2; i++)
         {
-            // Check whether the length of the char array is an odd number or even
-            // Then implement the required functionality based on them
-            if (s.Length % 2 == 1)
-            {
-                int middle = s.Length / 2;
-                if (i == middle)
-                {
-                    continue;
-                }
-
-                char temp = s[i];
-                s[i] = s[s.Length - (i + 1)];
-                s[s.Length - (i + 1)] = temp;
-            }
+            char temp = s[i];
+            s[i] = s[s.Length - (i + 1)];
+            s[s.Length - (i + 1)] = temp;
         }
 
         return s;
