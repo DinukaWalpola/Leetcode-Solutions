c8d81349a6c0565d6d841221df92369a419eb927
DinukaWalpola <dinukawalpola1@gmail.com>
2024-06-16 22:06:44 +0530

    Started working on LengthOfLastWordSolution

---
+++ LeetCode/Easy/LengthOfLastWordSolution.cs
@@ -0,0 +1,10 @@
+namespace LeetCode.Easy;
+
+public class LengthOfLastWordSolution
+{
+    public static void LengthOfLastWord(string s) {
+        // Try using a for loop (initial thought was nested loops)
+        // Create a string var outside the for loops to store the maximum length
+        // then compare it with each word in the loops 
+    }
+}
