387df6f84279fb05ff13a64feaa555aba79da785
DinukaWalpola <dinukawalpola1@gmail.com>
2024-08-02 23:18:38 +0530

    Removed commented code from the Maximum Number of Words Found in Sentences soluion

--- LeetCode/Easy/MaximumNumberOfWordsInASentenceSolution.cs
+++ LeetCode/Easy/MaximumNumberOfWordsInASentenceSolution.cs
@@ -5,24 +5,6 @@ public class MaximumNumberOfWordsInASentenceSolution
     public static int MostWordsFound(string[] sentences)
     {
         int wordCount = 0;
-
-        // foreach (var sent in sentences)
-        // {
-        //     int currentWordCount = 0;
-        //
-        //     for (int i = 0; i < sent.Length; i++)
-        //     {
-        //         if (sent[i] == ' ' || i == sent.Length - 1)
-        //         {
-        //             currentWordCount++;
-        //         }
-        //     }
-        //
-        //     if (currentWordCount > wordCount)
-        //     {
-        //         wordCount = currentWordCount;
-        //     }
-        // }
         
         foreach (var sentence in sentences)
         {
