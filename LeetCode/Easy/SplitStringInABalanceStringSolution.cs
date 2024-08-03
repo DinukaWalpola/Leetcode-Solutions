374d517d9e8cad756b2a9ecb4ba9df4fbda0c8aa
DinukaWalpola <dinukawalpola1@gmail.com>
2024-08-03 22:33:30 +0530

    Solved the Split a String in Balanced Strings problem and fixed all git issues that had previously

--- LeetCode/Easy/SplitStringInABalanceStringSolution.cs
+++ LeetCode/Easy/SplitStringInABalanceStringSolution.cs
@@ -4,52 +4,23 @@ public class SplitStringInABalanceStringSolution
 {
     public static int BalancedStringSplit(string s)
     {
+        int count = 0;
         int pairCount = 0;
-        int currentCount = 0;
-        string pairFirstLetter = "";
-        bool isMatching = false;
 
         for (int i = 0; i < s.Length; i++)
         {
-            if (!pairFirstLetter.Equals(""))
+            if (s[i] == 'R')
             {
-                Console.WriteLine($"{i}, pairFirstLetter: {pairFirstLetter}, s[i].ToString(): {s[i].ToString()}");
-                
-                // Console.WriteLine($"{i}, pairFirstLetter: {pairFirstLetter} ({(int)pairFirstLetter[0]}), s[i].ToString(): {s[i].ToString()} ({(int)s[i]})");
-
-                if (pairFirstLetter.Equals(s[i].ToString()))
-                {
-                    currentCount++;
-                }
-                else
-                {
-                    for (int j = i; j < i + currentCount; j++)
-                    {
-                        if (!pairFirstLetter.Equals(s[j].ToString()))
-                        {
-                            isMatching = true;
-                        }
-                        else
-                        {
-                            isMatching = false;
-                            break;
-                        }
-                    }
-                    
-                    if (isMatching)
-                    {
-                        pairCount++;
-                    }
-                    
-                    isMatching = false;
-                    currentCount = 0;
-                    pairFirstLetter = "";
-                }
+                count++;
+            } 
+            else if (s[i] == 'L')
+            {
+                count--;
             }
-            else
+
+            if (count == 0)
             {
-                pairFirstLetter = s[i].ToString();
-                currentCount++;
+                pairCount++;
             }
         }
         
