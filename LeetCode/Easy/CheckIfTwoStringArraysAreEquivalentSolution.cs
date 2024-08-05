377a13743984132ffc7f2ea4883ddee71ab1c52b
DinukaWalpola <dinukawalpola1@gmail.com>
2024-08-05 22:37:51 +0530

    Improved the solution for the Check If Two String Arrays are Equivalent problem

--- LeetCode/Easy/CheckIfTwoStringArraysAreEquivalentSolution.cs
+++ LeetCode/Easy/CheckIfTwoStringArraysAreEquivalentSolution.cs
@@ -4,22 +4,11 @@ public class CheckIfTwoStringArraysAreEquivalentSolution
 {
     public static bool ArrayStringsAreEqual(string[] word1, string[] word2)
     {
-        string w1 = String.Join("", word1);
-        string w2 = String.Join("", word2);
+        string w1 = String.Concat(word1);
+        string w2 = String.Concat(word2);
 
-        if (w1.Length != w2.Length) return false;
+        if (w1.Equals(w2)) return true;
 
-        char[] arr1 = w1.ToCharArray();
-        char[] arr2 = w2.ToCharArray();
-
-        for (int i = 0; i < arr1.Length; i++)
-        {
-            if (arr1[i] != arr2[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
+        return false;
     }
 }
