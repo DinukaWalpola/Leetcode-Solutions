082f8b230bee999f8481d1b049884768f25bd69c
DinukaWalpola <dinukawalpola1@gmail.com>
2024-08-09 15:11:43 +0530

    Solved the Decode The Message problem

---
+++ LeetCode/Easy/DecodeTheMessageSolution.cs
@@ -0,0 +1,29 @@
+using System.Text;
+
+namespace LeetCode.Easy;
+
+public class DecodeTheMessageSolution
+{ 
+    public static string DecodeMessage(string key, string message)
+    {
+        List<char> filteredKey = key.ToCharArray().Distinct().ToList();
+        filteredKey.Remove(' ');
+        char[] alphabet = Enumerable.Range('a', 26).Select(n => (char)n).ToArray();
+        StringBuilder decodedMessage = new StringBuilder(message.Length);
+
+        foreach (var m in message)
+        {
+            if (m != ' ')
+            {
+                int indexOfFilteredKey = filteredKey.IndexOf(m);
+                decodedMessage.Append(alphabet[indexOfFilteredKey]);
+            }
+            else
+            {
+                decodedMessage.Append(m);
+            }
+        }
+
+        return decodedMessage.ToString();
+    }
+}
