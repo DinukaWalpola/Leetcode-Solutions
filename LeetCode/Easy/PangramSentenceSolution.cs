09799eba8c8bb84fcb2c15a0825c58ee431eb609
DinukaWalpola <dinukawalpola1@gmail.com>
2024-08-18 12:40:52 +0530

    Solved the Check if the Sentence Is Pangram problem

---
+++ LeetCode/Easy/PangramSentenceSolution.cs
@@ -0,0 +1,24 @@
+namespace LeetCode.Easy;
+
+public class PangramSentenceSolution
+{
+    public static bool CheckIfPangram(string sentence)
+    {
+        List<char> characters = new List<char>();
+
+        foreach (var c in sentence)
+        {
+            if (!characters.Contains(c))
+            {
+                characters.Add(c);
+            }
+
+            if (characters.Count == 26)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
