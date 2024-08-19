72222fa347cb17d62fb114e970bfdeed160b8997
DinukaWalpola <dinukawalpola1@gmail.com>
2024-08-19 12:25:17 +0530

    Improved the solution for the Sorting the Sentence problem

--- LeetCode/Easy/SortingTheSentenceSolution.cs
+++ LeetCode/Easy/SortingTheSentenceSolution.cs
@@ -6,10 +6,11 @@ public class SortingTheSentenceSolution
 {
     public static string SortSentence(string s)
     {
-        Stack<string> words = new Stack<string>();
-        Stack<int> wordIndexes = new Stack<int>();
+        // Stack<string> words = new Stack<string>();
+        // Stack<int> wordIndexes = new Stack<int>();
         List<char> letters = new List<char>();
         string[] sortedSentence;
+        Dictionary<int, string> wordDictionary = new Dictionary<int, string>();
 
         for (int i = 0; i < s.Length; i++)
         {
@@ -19,8 +20,9 @@ public class SortingTheSentenceSolution
                 int index = Convert.ToInt32(indexVal);
                 letters.RemoveAt(letters.Count - 1);
                 // sortedSentence.Insert(index - 1, string.Join("", letters));
-                words.Push(string.Join("", letters));
-                wordIndexes.Push(index);
+                // words.Push(string.Join("", letters));
+                // wordIndexes.Push(index);
+                wordDictionary.Add(index, string.Join("", letters));
                 letters.Clear();
                 continue;
             }
@@ -28,18 +30,25 @@ public class SortingTheSentenceSolution
             {
                 double indexVal = Char.GetNumericValue(s[i]);
                 int index = Convert.ToInt32(indexVal);
-                words.Push(string.Join("", letters));
-                wordIndexes.Push(index);
+                // words.Push(string.Join("", letters));
+                // wordIndexes.Push(index);
+                wordDictionary.Add(index, string.Join("", letters));
                 break;
             }
             letters.Add(s[i]);
         }
 
-        sortedSentence = new string[words.Count];
+        // sortedSentence = new string[words.Count];
+        sortedSentence = new string[wordDictionary.Count];
         
-        foreach (var ind in wordIndexes)
+        // foreach (var ind in wordIndexes)
+        // {
+        //     sortedSentence[ind - 1] = words.Pop();
+        // }
+        
+        foreach (var keyValuePair in wordDictionary)
         {
-            sortedSentence[ind - 1] = words.Pop();
+            sortedSentence[keyValuePair.Key - 1] = keyValuePair.Value;
         }
 
         return string.Join(" ", sortedSentence);
