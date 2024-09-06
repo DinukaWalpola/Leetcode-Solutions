c305144179fd506e33e205740070ffeb5d82b77b
DinukaWalpola <dinukawalpola1@gmail.com>
2024-09-06 23:03:26 +0530

    Improved the solution for the Counting Words With A Given Prefix problem

--- LeetCode/Easy/CountingWordsWithAGivenPrefixSolution.cs
+++ LeetCode/Easy/CountingWordsWithAGivenPrefixSolution.cs
@@ -5,27 +5,12 @@ public class CountingWordsWithAGivenPrefixSolution
     public static int PrefixCount(string[] words, string pref)
     {
         int count = 0;
-        bool isMatching = true;
 
         for (int i = 0; i < words.Length; i++)
         {
             if (pref.Length > words[i].Length) continue;
-            
-            for (int j = 0; j < pref.Length; j++)
-            {
-                if (words[i][j] != pref[j])
-                {
-                    isMatching = false;
-                    break;
-                }
-            }
 
-            if (isMatching)
-            {
-                count++;
-            }
-
-            isMatching = true;
+            if (words[i].Substring(0, pref.Length).Equals(pref)) count++;
         }
 
         return count;
