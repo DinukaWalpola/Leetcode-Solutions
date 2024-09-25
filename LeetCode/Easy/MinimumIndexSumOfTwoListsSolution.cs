d4531053606f56aeb0666c26319dba3c35b05b1c
DinukaWalpola <dinukawalpola1@gmail.com>
2024-09-26 00:15:01 +0530

    Improved the solution of the Minimum Index Sum of Two Lists problem

--- LeetCode/Easy/MinimumIndexSumOfTwoListsSolution.cs
+++ LeetCode/Easy/MinimumIndexSumOfTwoListsSolution.cs
@@ -4,8 +4,8 @@ public class MinimumIndexSumOfTwoListsSolution
 {
     public static string[] FindRestaurant(string[] list1, string[] list2)
     {
-        Dictionary<string, int> duplicates = new Dictionary<string, int>();
         List<string> minIndex = new List<string>();
+        int min = int.MaxValue;
 
         for (int i = 0; i < list1.Length; i++)
         {
@@ -13,26 +13,18 @@ public class MinimumIndexSumOfTwoListsSolution
             {
                 if (list1[i].Equals(list2[j]))
                 {
-                    duplicates.Add(list1[i], i + j);
+                    if (i + j < min)
+                    {
+                        minIndex.Clear();
+                        min = i + j;
+                        minIndex.Add(list1[i]);
+                    }
+                    else if (i + j == min)
+                    {
+                        minIndex.Add(list1[i]);
+                    }
                     break;
                 }
-            }
-        }
-
-        int min = duplicates.Values.First();
-        minIndex.Add(duplicates.Keys.First());
-
-        for (int i = 1; i < duplicates.Values.Count; i++)
-        {
-            if (duplicates.ElementAt(i).Value < min)
-            {
-                minIndex.Clear();
-                min = duplicates.ElementAt(i).Value;
-                minIndex.Add(duplicates.ElementAt(i).Key);
-            }
-            else if (duplicates.ElementAt(i).Value == min)
-            {
-                minIndex.Add(duplicates.ElementAt(i).Key);
             }
         }
 
